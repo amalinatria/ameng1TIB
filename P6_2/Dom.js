0c1e12b098cf8af5a4d081540589aa5d93ba2524
dzaid25ti <amalina25ti@mahasiswa.pcr.ac.id>
2026-04-22 18:00:36 +0700

    Initial commit

---
+++ P6_2/Dom.js
@@ -0,0 +1,8 @@
+const btn = document.getElementById("btn");
+const judul = document.getElementById("judul");
+
+btn.addEventListener("click", 
+    function(){
+        judul.innerHTML=("Berubah nich");
+    }
+);
